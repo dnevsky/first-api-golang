49caa83118d689f021b27245aff629c1f1aafa41
dnevsky <asdzxc231111@gmail.com>
2023-04-19 18:43:25 +0300

    add handlers

--- cmd/main.go
+++ cmd/main.go
@@ -4,12 +4,14 @@ import (
 	"log"
 
 	"github.com/dnevsky/firstapi"
+	"github.com/dnevsky/firstapi/pkg/handler"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(firstapi.Server)
 
-	if err := srv.Run("8080"); err != nil {
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error while start http server: %s", err.Error())
 	}
 
