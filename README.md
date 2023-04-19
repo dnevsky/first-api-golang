8dc1613116aaec76fe1d27ead563a746a2cc869c
dnevsky <asdzxc231111@gmail.com>
2023-04-19 19:14:20 +0300

    added dependencies between resository, services and handlers

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 The first attempts to write a RESTful api server on golang while maintaining the principle of a clean project architecture.
+
--- cmd/main.go
+++ cmd/main.go
@@ -5,12 +5,16 @@ import (
 
 	"github.com/dnevsky/firstapi"
 	"github.com/dnevsky/firstapi/pkg/handler"
+	"github.com/dnevsky/firstapi/pkg/repository"
+	"github.com/dnevsky/firstapi/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
-	srv := new(firstapi.Server)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
+	srv := new(firstapi.Server)
 	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error while start http server: %s", err.Error())
 	}
