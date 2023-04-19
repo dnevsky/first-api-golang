0a4b1eb4fb910be3d9c93487f1d983b670d85620
dnevsky <asdzxc231111@gmail.com>
2023-04-19 20:48:48 +0300

    add postgres driver, read env variables

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/dnevsky/firstapi"
 	"github.com/dnevsky/firstapi/pkg/handler"
 	"github.com/dnevsky/firstapi/pkg/repository"
 	"github.com/dnevsky/firstapi/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 )
 
@@ -14,7 +17,24 @@ func main() {
 	if err := initConfig(); err != nil {
 		log.Fatalf("error init configs: %s", err.Error())
 	}
-	repos := repository.NewRepository()
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env vars: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatalf("failed to init db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
